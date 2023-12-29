82e55d075687e06e84894ff33825cd3d0085ce23
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-12-29 15:35:08 +0800

    优化重置对话时钟 自动重载配置

--- me.cqp.luohuaming.ChatGPT.Code/Event_StartUp.cs
+++ me.cqp.luohuaming.ChatGPT.Code/Event_StartUp.cs
@@ -1,14 +1,18 @@
 using me.cqp.luohuaming.ChatGPT.PublicInfos;
+using me.cqp.luohuaming.ChatGPT.PublicInfos.API;
 using me.cqp.luohuaming.ChatGPT.Sdk.Cqp.EventArgs;
 using me.cqp.luohuaming.ChatGPT.Sdk.Cqp.Interface;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Timers;
 
 namespace me.cqp.luohuaming.ChatGPT.Code
 {
     public class Event_StartUp : ICQStartup
     {
+        public Timer ClearFlowTimer { get; private set; }
+
         public void CQStartup(object sender, CQStartupEventArgs e)
         {
             MainSave.AppDirectory = e.CQApi.AppDirectory;
@@ -17,22 +21,67 @@ namespace me.cqp.luohuaming.ChatGPT.Code
             MainSave.ImageDirectory = CommonHelper.GetAppImageDirectory();
             MainSave.CurrentQQ = e.CQApi.GetLoginQQ();
             ConfigHelper.ConfigFileName = Path.Combine(MainSave.AppDirectory, "Config.json");
+            if (ConfigHelper.Load() is false)
+            {
+                MainSave.CQLog.Warning("加载配置文件", "内容格式不正确，无法加载");
+            }
             foreach (var item in Assembly.GetAssembly(typeof(Event_GroupMessage)).GetTypes())
             {
                 if (item.IsInterface)
+                {
                     continue;
+                }
+
                 foreach (var instance in item.GetInterfaces())
                 {
                     if (instance == typeof(IOrderModel))
                     {
                         IOrderModel obj = (IOrderModel)Activator.CreateInstance(item);
                         if (obj.ImplementFlag == false)
+                        {
                             break;
+                        }
+
                         MainSave.Instances.Add(obj);
                     }
                 }
             }
             AppConfig.Init();
+            ClearFlowTimer = new Timer();
+            ClearFlowTimer.Elapsed += ClearFlowTimer_Elapsed;
+            ClearFlowTimer.Interval = 1000;
+            ClearFlowTimer.Start();
+
+            FileSystemWatcher configChangeWatcher = new FileSystemWatcher();
+            configChangeWatcher.Path = Path.GetDirectoryName(ConfigHelper.ConfigFileName);
+            configChangeWatcher.Filter = Path.GetFileName(ConfigHelper.ConfigFileName);
+            configChangeWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            configChangeWatcher.Changed += ConfigChangeWatcher_Changed;
+            configChangeWatcher.EnableRaisingEvents = true;
+
+            MainSave.CQLog.Info("初始化", "ChatGPT插件初始化完成");
+        }
+
+        private void ConfigChangeWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed && ConfigHelper.Load())
+            {
+                AppConfig.Init();
+            }
+        }
+
+        private void ClearFlowTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < Chat.ChatFlows.Count; i++)
+            {
+                var item = Chat.ChatFlows[i];
+                item.RemoveTimeout++;
+                if (item.RemoveTimeout >= AppConfig.ChatTimeout)
+                {
+                    item.RemoveFromFlows();
+                    i--;
+                }
+            }
         }
     }
 }
