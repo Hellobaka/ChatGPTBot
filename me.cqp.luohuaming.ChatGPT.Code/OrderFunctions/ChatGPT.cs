d2b490ffee6241ac98dcdabec533bb3a9db9a7b9
Mr.喜 <863450594@qq.com>
2025-03-06 10:28:44 +0800

    扩展ATCQ码检查

--- me.cqp.luohuaming.ChatGPT.Code/OrderFunctions/ChatGPT.cs
+++ me.cqp.luohuaming.ChatGPT.Code/OrderFunctions/ChatGPT.cs
@@ -2,6 +2,7 @@ using me.cqp.luohuaming.ChatGPT.PublicInfos;
 using me.cqp.luohuaming.ChatGPT.PublicInfos.API;
 using me.cqp.luohuaming.ChatGPT.Sdk.Cqp;
 using me.cqp.luohuaming.ChatGPT.Sdk.Cqp.EventArgs;
+using me.cqp.luohuaming.ChatGPT.Sdk.Cqp.Model;
 using System;
 using System.IO;
 using System.Linq;
@@ -21,6 +22,8 @@ namespace me.cqp.luohuaming.ChatGPT.Code.OrderFunctions
 
         private Regex ReplyPattern { get; set; } = new Regex(Regex.Escape("[CQ:reply,id=") + @"(\d+)\]");
 
+        private Regex AtPattern { get; set; } = new Regex("\\[CQ:at,qq=(\\d*).*?\\]");
+
         public FunctionResult Progress(CQGroupMessageEventArgs e)
         {
             string message = e.Message.Text;
@@ -28,17 +31,18 @@ namespace me.cqp.luohuaming.ChatGPT.Code.OrderFunctions
             {
                 return new FunctionResult();
             }
-            string atCQCode = CQApi.CQCode_At(MainSave.CurrentQQ).ToString();
+
             if (Chat.ChatFlows.Any(x => x.Id == e.FromQQ && x.ContinuedMode)
                 || (message.Replace("＃", "#").StartsWith(GetOrderStr()))
-                || (AppConfig.AtResponse && ((AppConfig.AtAnyPosition && message.Contains(atCQCode)) || message.StartsWith(atCQCode)))
-                || (AppConfig.ReplyResponse && message.Contains("[CQ:reply,id=") && message.Contains(atCQCode)))
+                || (AppConfig.AtResponse && ((AppConfig.AtAnyPosition && CheckAt(e.Message.Text, false)) || CheckAt(e.Message.Text, true)))
+                || (AppConfig.ReplyResponse && message.Contains("[CQ:reply,id=") && CheckAt(e.Message.Text, false)))
             {
                 if (!string.IsNullOrWhiteSpace(AppConfig.WelcomeText))
                 {
                     Record.RecordSelfMessage(e.FromGroup, e.FromGroup.SendGroupMessage(AppConfig.WelcomeText));
                 }
-                message = message.Replace("＃", "#").Replace(GetOrderStr(), "").Replace(atCQCode, "");
+                message = message.Replace("＃", "#").Replace(GetOrderStr(), "");
+                message = AtPattern.Replace(message, "");
                 if (AppConfig.ReplyResponse)
                 {
                     foreach (Match item in ReplyPattern.Matches(message))
@@ -144,6 +148,32 @@ namespace me.cqp.luohuaming.ChatGPT.Code.OrderFunctions
             }
             result.SendObject.Add(sendText);
             return result;
+        }
+
+        private bool CheckAt(string input, bool forceBegin)
+        {
+            // 要求CQ码必须在开头, 所以只检查原始文本开头是否为At CQ码即可
+            if (forceBegin && input.StartsWith("[CQ:at"))
+            {
+                return false;
+            }
+
+            var cqcodes = CQCode.Parse(input);
+            // 获取所有At CQ码
+            var atCode = cqcodes.Where(x => x.Function == Sdk.Cqp.Enum.CQFunction.At);
+            // 未查询到返回false
+            if (atCode == null || atCode.Count() == 0)
+            {
+                return false;
+            }
+            // 强制开头检查 取第一个CQ码 检查QQ是否为本机QQ
+            if (forceBegin)
+            {
+                return atCode.FirstOrDefault()?.Items["qq"] == MainSave.CurrentQQ.ToString();
+            }
+
+            // 非强制开头检查 检查第一个QQ为本机QQ是否存在
+            return atCode.Any(x => x.Items["qq"] == MainSave.CurrentQQ.ToString());
         }
     }
 }
