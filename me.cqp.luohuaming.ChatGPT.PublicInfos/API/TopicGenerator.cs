27eea8f916f78d8df478a6d81b8a6eb4c16bb7b2
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2025-03-30 19:07:09 +0800

    过滤话题输出

--- me.cqp.luohuaming.ChatGPT.PublicInfos/API/TopicGenerator.cs
+++ me.cqp.luohuaming.ChatGPT.PublicInfos/API/TopicGenerator.cs
@@ -5,7 +5,7 @@ namespace me.cqp.luohuaming.ChatGPT.PublicInfos.API
 {
     public static class TopicGenerator
     {
-        public const string Prompt = "我会为你提供一段话，这段话是聊天记录。请你从后续提供的一段话中总结出{0}个关键的概念，可以是名词，动词，或者特定人物，帮我列出来，用逗号`,`隔开，尽可能精简。只需要列举{0}个话题就好，不要有序号，不要告诉我其他内容。\n这是文本输入：{1}";
+        public const string Prompt = "我会为你提供一段话，这段话是聊天记录。请你从后续提供的一段话中总结出{0}个关键的概念，可以是名词，动词，或者特定人物，帮我列出来，用逗号`,`隔开，尽可能精简。只需要列举{0}个话题就好，不要有序号，不要告诉我其他内容，一定只需要结果，一定不要输出任何解释文本，例如`好的，以下是...`。\n这是文本输入：{1}";
 
         private static string[] Filter { get; set; } = ["昵称", "图片", "聊天记录", "QuoteMessage"];
 
@@ -18,7 +18,7 @@ namespace me.cqp.luohuaming.ChatGPT.PublicInfos.API
             if (raw != Chat.ErrorMessage)
             {
                 raw = raw.Replace("，", ",").Replace("、", ",").Replace(" ", ",");
-                return raw.Split(',').Take(AppConfig.TopicCount).Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x) && !Filter.Contains(x)).ToArray();
+                return raw.Split(',').Where(x => !string.IsNullOrEmpty(x) && !Filter.Contains(x)).Take(AppConfig.TopicCount).Select(x => x.Trim()).ToArray();
             }
             else
             {
