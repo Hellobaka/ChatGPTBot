2a4167d54d0f14ef3a50e8d1b6334bf684bebc28
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2025-03-30 17:53:17 +0800

    优化话题提取Prompt

--- me.cqp.luohuaming.ChatGPT.PublicInfos/API/TopicGenerator.cs
+++ me.cqp.luohuaming.ChatGPT.PublicInfos/API/TopicGenerator.cs
@@ -5,16 +5,25 @@ namespace me.cqp.luohuaming.ChatGPT.PublicInfos.API
 {
     public static class TopicGenerator
     {
-        public const string Prompt = "这是一段文字：{0}。请你从这段话中总结出{1}个关键的概念，可以是名词，动词，或者特定人物，帮我列出来，用逗号`,`隔开，尽可能精简。只需要列举{1}个话题就好，不要有序号，不要告诉我其他内容。";
+        public const string Prompt = "我会为你提供一段话，这段话是聊天记录。请你从后续提供的一段话中总结出{0}个关键的概念，可以是名词，动词，或者特定人物，帮我列出来，用逗号`,`隔开，尽可能精简。只需要列举{0}个话题就好，不要有序号，不要告诉我其他内容。\n这是文本输入：{1}";
 
-        public static string[] GetTopics(string input)
+        private static string[] Filter { get; set; } = ["昵称", "图片", "聊天记录", "QuoteMessage"];
+
+        public static string[]? GetTopics(string input)
         {
             string raw = Chat.GetChatResult(AppConfig.TopicUrl, AppConfig.TopicApiKey,
                 [
-                    new SystemChatMessage(string.Format(Prompt, input, AppConfig.TopicCount)),
+                    new SystemChatMessage(string.Format(Prompt, AppConfig.TopicCount, input)),
                 ], AppConfig.TopicModelName);
-            raw = raw.Replace("，", ",");
-            return raw.Split(',').Take(AppConfig.TopicCount).ToArray();
+            if (raw != Chat.ErrorMessage)
+            {
+                raw = raw.Replace("，", ",").Replace("、", ",").Replace(" ", ",");
+                return raw.Split(',').Take(AppConfig.TopicCount).Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x) && !Filter.Contains(x)).ToArray();
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
