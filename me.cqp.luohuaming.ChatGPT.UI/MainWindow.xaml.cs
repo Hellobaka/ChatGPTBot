0386d0591b534ea4079c5ff9490e6442b0c56dbb
Mr.喜 <863450594@qq.com>
2025-09-23 15:05:13 +0800

    集合部分页面到杂项页面

--- me.cqp.luohuaming.ChatGPT.UI/MainWindow.xaml.cs
+++ me.cqp.luohuaming.ChatGPT.UI/MainWindow.xaml.cs
@@ -62,48 +62,6 @@ namespace me.cqp.luohuaming.ChatGPT.UI
             return await dialog.ShowAsync() == ContentDialogResult.Primary;
         }
 
-        /// <summary>
-        /// 文本框删除点击时发光的外边框
-        /// </summary>
-        /// <param name="element"></param>
-        public static void SetElementNoSelectEffect(UIElement element)
-        {
-            UIElement? GetBorderElement(DependencyObject element)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
-                {
-                    var e = VisualTreeHelper.GetChild(element, i);
-                    if (e is Border border && border.Name == "BorderElement")
-                    {
-                        return border;
-                    }
-                    else if (e is Grid ignoreGrid && ignoreGrid.Name == "ContentRoot")
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        var result = GetBorderElement(e);
-                        if (result != null)
-                        {
-                            return result;
-                        }
-                    }
-                }
-                return null;
-            }
-
-            var borderElement = GetBorderElement(element);
-            if (borderElement != null)
-            {
-                if (borderElement is Border border)
-                {
-                    // 设置边框画笔为透明
-                    border.BorderBrush = Brushes.Transparent;
-                }
-            }
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             if (App.Debug)
---
+++ me.cqp.luohuaming.ChatGPT.UI/Pages/Misc.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace me.cqp.luohuaming.ChatGPT.UI.Pages
+{
+    /// <summary>
+    /// Misc.xaml 的交互逻辑
+    /// </summary>
+    public partial class Misc : Page
+    {
+        public Misc()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, object> PageCache { get; set; } = new();
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = e.AddedItems.Count > 0 ? e.AddedItems[0] : null;
+            if (selectedItem != null && selectedItem is TabItem tabItem)
+            {
+                string selectedItemTag = (string)tabItem.Tag;
+                if (PageCache.TryGetValue(selectedItemTag, out object? page))
+                {
+                    MiscFrame.Navigate(page);
+                }
+                else
+                {
+                    Type? pageType = typeof(MainWindow).Assembly.GetType("me.cqp.luohuaming.ChatGPT.UI.Pages." + selectedItemTag);
+                    if (pageType == null)
+                    {
+                        return;
+                    }
+                    var obj = Activator.CreateInstance(pageType);
+                    if (obj == null)
+                    {
+                        return;
+                    }
+                    PageCache.Add(selectedItemTag, obj);
+
+                    MiscFrame.Navigate(obj);
+                }
+            }
+        }
+    }
+}
