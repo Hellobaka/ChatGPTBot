856d3612bb7ad48dae31d42beeaec9c492d0faf2
Mr.喜 <863450594@qq.com>
2025-10-30 12:57:43 +0800

    Usage修复输入与缓存输入

--- me.cqp.luohuaming.ChatGPT.PublicInfos/API/ChatRelated/UsageTracker.cs
+++ me.cqp.luohuaming.ChatGPT.PublicInfos/API/ChatRelated/UsageTracker.cs
@@ -7,10 +7,10 @@ namespace me.cqp.luohuaming.ChatGPT.PublicInfos.API
     {
         public void TrackUsage(string baseUrl, LLMModel model, string purpose, UsageDetails usage, string apiKey)
         {
-            long inputTokenCount = usage.InputTokenCount ?? 0;
             long outputTokenCount = usage.OutputTokenCount ?? 0;
             long totalTokenCount = usage.TotalTokenCount ?? 0;
             long cachedTokenCount = usage.AdditionalCounts.TryGetValue("InputTokenDetails.CachedTokenCount", out long t) ? t : 0;
+            long inputTokenCount = (usage.InputTokenCount ?? 0) - cachedTokenCount;
             decimal consume = model.CalcConsume(inputTokenCount, outputTokenCount, totalTokenCount, cachedTokenCount);
             Usage.Insert(baseUrl, model.Name, purpose
                 , inputTokenCount
