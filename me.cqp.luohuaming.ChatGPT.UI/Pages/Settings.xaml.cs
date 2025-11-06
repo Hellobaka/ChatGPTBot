49e5d8979f8575a2c6b79f314448393e600d0332
Mr.喜 <863450594@qq.com>
2025-11-06 12:58:30 +0800

    TODO

--- me.cqp.luohuaming.ChatGPT.UI/Pages/Settings.xaml.cs
+++ me.cqp.luohuaming.ChatGPT.UI/Pages/Settings.xaml.cs
@@ -17,6 +17,7 @@ namespace me.cqp.luohuaming.ChatGPT.UI.Pages
     /// </summary>
     public partial class Settings : Page
     {
+        // TODO: 更详细的用途管理
         public Settings()
         {
             InitializeComponent();
